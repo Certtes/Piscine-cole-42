1ccc64465e4bd634326bb0fb305e45b090b25fe6
Certtes <127451842+Certtes@users.noreply.github.com>
2023-03-09 23:39:17 +0100

    Add files via upload

---
+++ ft_print_reverse_alphabet.c
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_reverse_alphabet.c                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tbourrec <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/08/09 23:07:15 by tbourrec          #+#    #+#             */
+/*   Updated: 2022/08/09 23:10:37 by tbourrec         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+#include <unistd.h>
+
+void	ft_print_reverse_alphabet(void)
+{
+	char	lettre;
+
+	lettre = 'z';
+	while (lettre >= 'a')
+	{
+		write(1, &lettre, 1);
+		lettre--;
+	}
+}
